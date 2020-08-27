f5d3037a0fdfefc975acc891d9271998324933f5
Manzoor Khan <66812571+manzoork@users.noreply.github.com>
2020-08-26 21:16:10 -0500

    Update RegularExpressions.py
     Finding Numbers in a Haystack
    
    In this assignment you will read through and parse a file with text and numbers. You will extract all the numbers in the file and compute the sum of the numbers

--- RegularExpressions.py
+++ RegularExpressions.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+#  Finding Numbers in a Haystack
+# In this assignment you will read through and parse a file with text and numbers. You will extract all the numbers in the file and compute the sum of the numbers
 """
 Created on Thu Jul 16 13:53:43 2020
 
